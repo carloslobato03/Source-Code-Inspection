b1032fb88695fb9f22dc03202ff078d25f0a5b24
31517072 <31517072@087997-P33S103.macklabs.br>
2016-09-30 10:30:40 -0300

    Teste TicketMachine Incompleto

---
+++ Source Code Inspection/test/TicketMachineTeste.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31517072
+ */
+public class TicketMachineTeste {
+    
+    @Test
+    public void inserirDinheiro() throws PapelMoedaInvalidaException{
+        TicketMachine machine = new TicketMachine(0);
+        machine.inserir(20);
+        assertEquals(machine.getSaldo(),20);
+        
+    }
+    public TicketMachineTeste() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
